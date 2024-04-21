5c1f078e8e22dfb92144ae8edddaedf88cf95b15
Ryan Ozawa <ryan.ozawa21@gmail.com>
2024-04-20 19:46:20 -0700

    learn: add more drizzle sub select via relations as exploratory

--- src/app/api/stats/players/route.ts
+++ src/app/api/stats/players/route.ts
@@ -14,13 +14,43 @@ const exampleData = {
             agentPlays: {
                 1: 1,
             },
+            // this can either be calculated here or in SQL
+            gamesPlayed: 1,
+            kdRatio: {
+                overall: 1.01134,
+                min: 1.01134,
+                max: 1.01134,
+            },
         },
     ],
     length: 10,
 };
 
 export async function GET(): Promise<NextResponse> {
-    const playerResult = await db.query.players.findMany();
+    const playerResult = await db.query.players.findMany({
+        columns: {
+            id: true,
+            name: true,
+            teamId: true,
+        },
+        with: {
+            playerMatchesRel: {
+                columns: {
+                    id: true,
+                    matchId: true,
+                    playerAgentId: true,
+                    matchPlace: true,
+                    playerKills: true,
+                    playerDeaths: true,
+                    playerAssists: true,
+                },
+                // limit: 1  // limit the relation return if wanted
+            },
+        },
+    });
+
     console.table(playerResult);
+    console.log(playerResult[0].playerMatchesRel);
+
     return new NextResponse(JSON.stringify(exampleData));
 }
